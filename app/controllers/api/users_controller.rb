90cbfe7022ae4da4f17c427c1a3fd6a2813ad224
Phuong Doan <phuong.doan@techpropulsionlabs.com>
2011-12-27 10:13:58 +0700

    [User management - API] login/logout

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,15 +1,40 @@
-class Api::UsersController < Api::BaseController
+class Api::UsersController < ApplicationController
+#  before_filter :authenticate_user!, :only => [:login]
+  
   def create_user
     info = params
     user = User.new info
     result = {
-      :status => true,
+      :success => true,
       :msg => {}
     }
     unless user.save
       result[:msg] = user.errors 
-      result[:status] = false
+      result[:success] = false
     end
+    render :json => { :info => result }
+  end
+  
+  def login 
+    result = {
+      :success => false,
+      :auth_token => ''
+    }
+    user = User.find_by_username params[:username]
+    if user && user.valid_password?(params[:password])
+      session[:user_id] = user.id
+      result[:success] = true
+      result[:auth_token] = user.ensure_authentication_token!
+    end
+    
+    render :json => { :info => result }
+  end
+  
+  def logout
+    user = current_user
+    user.authentication_token = nil
+    result[:success] = @user.save? true : false
+    
     render :json => { :info => result }
   end
 end
