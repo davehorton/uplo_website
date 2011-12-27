7988f2b5d2953dec6e51a7d3b50b40b54b267b53
Man Vuong <man.vuong@techpropulsionlabs.com>
2011-12-27 14:08:11 +0700

    Config for staging mode

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,4 @@
-FbIntranet::Application.configure do
+Uplo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
