b9a674309e196474848fe4c195d35d3a0f8e68ed
Man Vuong <man.vuong@techpropulsionlabs.com>
2011-12-27 13:56:33 +0700

    [User Management] Add seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+admin = User.create!({
+  :email => "admin@uplo.com",
+  :username => "admin",
+  :last_name => "Admin",
+  :first_name => "Super",
+  :password => "123456",
+  :password_confirmation => "123456",
+  :confirmation_token => nil,
+  :confirmed_at => Time.now, #bypass email confirmation
+  :confirmation_sent_at => Time.now, #bypass email confirmation
+})
+
+if admin.confirmed_at.blank?
+  admin.confirmed_at = Time.now
+  admin.save
+end
