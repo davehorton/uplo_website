a4d5fd4ab45051df9d3bcbe3f9e5323447b99409
Man Vuong <man.vuong@techpropulsionlabs.com>
2012-02-21 14:08:51 +0700

    #76: fix bug when using Forgot password

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -6,6 +6,16 @@ class Users::PasswordsController < ApplicationController
     render_with_scope :new
   end
   
+  def create
+    self.resource = resource_class.send_reset_password_instructions(params[resource_name])
+
+    if successfully_sent?(resource)
+      respond_with({}, :location => new_session_path(resource_name))
+    else
+      respond_with_navigational(resource){ render_with_scope :new }
+    end
+  end
+  
   def edit
     @user = current_user
   end
