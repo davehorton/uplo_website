0a7d20e8c8130c81d7d169a2a97b1c1008b03791
Phuong Doan <phuong.doan@techpropulsionlabs.com>
2012-03-27 15:21:45 +0700

    [Layout] refine css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,12 @@
 class HomeController < ApplicationController
-  before_filter :authenticate_user!, :only => [:browse]
+  before_filter :authenticate_user!, :except => [:index]
 
   def index
     session[:back_url] = url_for(:controller => 'home', :action => "browse") if session[:back_url].nil?
     @images = Image.load_popular_images(@filtered_params)
+    if user_signed_in?
+      redirect_to :action => :popular
+    end
   end
 
   def browse
