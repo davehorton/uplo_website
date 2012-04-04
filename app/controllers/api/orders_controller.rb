a781b2037e7cd909a096274e445d5fcbd1b331e7
Phuong Doan <phuong.doan@techpropulsionlabs.com>
2012-04-04 09:43:03 +0700

    [API - Shopping cart] build orderred item

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,28 +1,29 @@
 class Api::OrdersController < Api::BaseController
   before_filter :require_login!
-  
+
   # GET: /api/list_orders
-  # params: 
+  # params:
   # result:
-  # 
+  #
   def list_orders
     orders = @user.orders.load_orders(@filtered_params)
-    @result[:total] = orders.total_entries    
+    @result[:total] = orders.total_entries
     @result[:data] = orders_to_json(orders)
     @result[:success] = true
     render :json => @result
   end
-  
+
   # POST: /api/create_order
-  # params: 
-  # {order => {:images => [1, 2, 3, 4, 5], :transaction_code => "*", :transaction_status => "*"}}
+  # params:
+  # {order => {:images => "[{"image_id":1, "plexi_mount":"true", "size":"500x500", "moulding":"Wood", "quantity":"5"}]",
+  #            :transaction_code => "*", :transaction_status => "*"}
   def create_order
     order_info = params[:order]
-    image_ids = order_info.delete(:images)
+    images = order_info.delete(:images)
     order_info[:user] = @user
     order = Order.new(order_info)
-    order.line_items = build_order_items(image_ids)
-    
+    order_items = build_order_items(JSON.parse(images))
+
     done = false
     if order.valid?
       order.compute_totals
@@ -31,51 +32,52 @@ class Api::OrdersController < Api::BaseController
         @result[:success] = true
         done = true
       elsif order.save
+        order.line_items << order_items
         @result[:order_id] = order.id
         @result[:success] = true
         done = true
       end
     end
-    
+
     if !done
-      @result[:msg] = order.errors 
+      @result[:msg] = order.errors
     end
-    
+
     return render :json => @result
   end
-  
+
   protected
-  
-  def build_order_items(image_ids)
+  # items: [{"image_id":1, "plexi_mount":"true", "size":"500x500", "moulding":"Wood", "quantity":"5"}]
+  def build_order_items(items)
     items = []
-    images = Image.where(:id => image_ids)
-    images.each do |image|
-      items << LineItem.new({
-        :image => image,
-        :price => image.price        
-      })
+    items.each do |item_info|
+      line_item = LineItem.new do |item|
+        item.attributes = item_info
+        item.price = (image.price.nil? ? '0' : image.price)
+        items << item
+      end
     end
     return items
   end
-  
+
   def orders_to_json(orders)
     json_array = []
-    
+
     orders.each do |order|
       data = {}
-      data[:order] = order.serializable_hash({ 
+      data[:order] = order.serializable_hash({
         :except => order.except_attributes,
-        :methods => order.exposed_methods, 
+        :methods => order.exposed_methods,
       })
 
       data[:order][:images] = []
       order.images.each do |img|
         data[:order][:images] << img.serializable_hash(img.default_serializable_options)
       end
-      
+
       json_array << data
     end
-    
+
     return json_array
   end
 end
