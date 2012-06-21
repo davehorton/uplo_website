1c2965d3bb09562840f3fc332238d370a31780f1
Phuong Doan <phuong.doan@techpropulsionlabs.com>
2012-06-21 16:22:41 +0700

    [API][User] number of user's galleries

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -172,6 +172,8 @@ class Api::UsersController < Api::BaseController
     info[:avatar_url] = user.avatar_url
     info[:followers_num] = user.followers.size
     info[:following_num] = user.followed_users.size
+    info[:galleries_num] = user.galleries.size
+    info[:images_num] = user.images.size
 
     return info
   end
