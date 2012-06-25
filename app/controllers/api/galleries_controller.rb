5c4d0614e79c70ef4ed14ba269a381437be317cb
Phuong Doan <phuong.doan@techpropulsionlabs.com>
2012-06-25 10:54:34 +0700

    [API][Gallery] default cover image for gallery

--- app/controllers/api/galleries_controller.rb
+++ app/controllers/api/galleries_controller.rb
@@ -166,6 +166,8 @@ class Api::GalleriesController < Api::BaseController
       if data[:gallery][:cover_image]
         data[:gallery][:cover_image] = data[:gallery][:cover_image].serializable_hash(Image.default_serializable_options)
         data[:gallery][:updated_at] = gallery.updated_at.strftime('%m/%d/%y')
+      else
+        data[:gallery][:cover_image] = {:image_thumb_url => "http://#{DOMAIN}/assets/gallery-thumb-180.jpg"}
       end
 
       json_array << data
