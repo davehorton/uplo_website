756e8b152209b95bedb0f8b67e067278547687c7
Man Vuong <man.vuong@techpropulsionlabs.com>
2012-07-23 19:40:21 +0700

    #465, #466: enhance Sort By selection

--- db/migrate/20120720105504_add_pageview_to_images.rb
+++ db/migrate/20120720105504_add_pageview_to_images.rb
@@ -1,5 +1,5 @@
 class AddPageviewToImages < ActiveRecord::Migration
   def change
-    add_column :images, :pageview, :integer
+    add_column :images, :pageview, :integer, :default => 0
   end
 end
