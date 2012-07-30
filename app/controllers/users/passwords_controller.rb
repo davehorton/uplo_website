c34817424d58e5b918038353abf63c7298d8c9d6
An Nguyen <an.nguyen@techpropulsionlabs.com>
2012-07-30 19:06:56 +0700

    add mobile web for reset password

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,4 +1,6 @@
 class Users::PasswordsController < Devise::PasswordsController
+  has_mobile_fu
+  
   prepend_before_filter :require_no_authentication, :except => [:edit_password, :update_password]
   before_filter :authenticate_user!, :only => [:edit_password, :update_password]
 
@@ -15,6 +17,30 @@ class Users::PasswordsController < Devise::PasswordsController
       render :action => :edit_password
     end
   end
+  
+  # OVERRIDE METHODS
+  
+  # PUT /resource/password
+   def update
+     self.resource = User.reset_password_by_token(params[:user])
+
+     if resource.errors.empty?
+       flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+       set_flash_message(:notice, flash_message) if is_navigational_format?
+       sign_in(resource_name, resource)
+       if(in_mobile_view?)
+         render :finish_reset
+       else
+         respond_with resource, :location => after_sign_in_path_for(resource)
+        end
+     else
+       if(in_mobile_view?)
+         render :action => :edit
+       else
+         respond_with resource
+       end
+     end
+   end
 
   protected
 
