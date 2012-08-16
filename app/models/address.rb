2e2a6c866741ebbcb2cf3163176ba02740d561a2
An Nguyen <an.nguyen@techpropulsionlabs.com>
2012-08-16 18:21:43 +0700

    update

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,5 @@
 class Address < ActiveRecord::Base
 	validates_presence_of :first_name, :last_name, :address, :city, :state, :zip_code
+	validates_numericality_of :zip_code, :only_integer => true
+	validates_length_of :zip_code, :is => 5
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -76,10 +76,12 @@ class User < ActiveRecord::Base
   validates :password, :presence => true, :confirmation => true, :unless => :force_submit
   validates_format_of :website, :allow_blank => true,
           :with => /(^$)|(^((http|https):\/\/){0,1}[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/i
-  validates :email, :presence => true, :email => true
+  validates :email, :email => true
   validates_uniqueness_of :username, :message => 'must be unique'
   validates_length_of :first_name, :last_name, :in => 2..30, :message => 'must be 2 - 30 characters in length'
   validates_confirmation_of :paypal_email, :message => "should match confirmation"
+  validates_length_of :cvv, :is => 3
+  validates_numericality_of :cvv, :card_number, :only_integer => true
   validates_presence_of :paypal_email_confirmation, :if => :paypal_email_changed?
   validates :paypal_email, :email => true, :if => :paypal_email_changed?
   # SCOPE
