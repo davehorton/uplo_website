7fc503517f513a3cd9850c1128882630db841d57
An Nguyen <an.nguyen@techpropulsionlabs.com>
2012-08-21 17:01:10 +0700

    fake time date

--- app/models/order.rb
+++ app/models/order.rb
@@ -122,7 +122,7 @@ class Order < ActiveRecord::Base
   protected
     def init_transaction_date
       if self.transaction_date.blank?
-        self.transaction_date = Time.now
+        self.transaction_date = Date.parse("01/07/2012")
       end
       return self.transaction_date
     end
