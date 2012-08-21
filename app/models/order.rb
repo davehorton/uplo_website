c8b79a538425b10879a6f7ad649026f1f230145f
An Nguyen <an.nguyen@techpropulsionlabs.com>
2012-08-21 17:10:38 +0700

    change date for testing

--- app/models/order.rb
+++ app/models/order.rb
@@ -122,7 +122,7 @@ class Order < ActiveRecord::Base
   protected
     def init_transaction_date
       if self.transaction_date.blank?
-        self.transaction_date = Date.parse("01/07/2012")
+        self.transaction_date = Date.parse "01/11/2011"
       end
       return self.transaction_date
     end
