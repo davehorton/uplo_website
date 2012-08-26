cc8767ba42be5f22f099c9967130461c6172098f
Man Vuong <man.vuong@techpropulsionlabs.com>
2012-08-27 02:44:32 +0700

    #583: show authenticate error message on the login form. Refactor search box and notification box

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,21 +1,12 @@
 class Users::SessionsController < Devise::SessionsController
-  #ssl_required :sign_in, :new, :create if ssl_required?
-
   skip_before_filter(:authenticate_user!)
   skip_authorization_check
-  layout false
 
-  def set_current_tab
-    # bypass this method since login page does not contains any tabs
-  end
-
+  # GET /resource/sign_in
   def new
-    flash[:info] = 'You have to sign in first!'
-    redirect_to '/'
-  end
-
-  # POST /resource/sign_in
-  def create
-    super
+    session[:devise_message] = flash.now[:alert]
+    # Cleanup the flash
+    flash.now[:alert] = nil
+    redirect_to "/"
   end
 end
