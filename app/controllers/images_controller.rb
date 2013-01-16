09ab2691bc377d2806a6b6c68b51fd867804e231
Phuong Doan <phuong.doan@techpropulsionlabs.com>
2013-01-16 11:35:53 +0700

    [image] upload size error

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -86,9 +86,9 @@ class ImagesController < ApplicationController
       mb_unit = FileSizeConverter::UNITS[:megabyte]
       mb_img_size = FileSizeConverter.convert img_size, FileSizeConverter::UNITS[:byte], mb_unit
       free_allocation = [0, (FileSizeConverter.convert current_user.free_allocation, FileSizeConverter::UNITS[:byte], mb_unit)].max
-      result = [{:error => "UPLOAD FAILED! This image is #{mb_img_size} #{mb_unit.upcase}. You have only #{free_allocation} #{mb_unit.upcase} / #{User::ALLOCATION_STRING} free" }]
+      result = {:success => false, :msg => "UPLOAD FAILED! This image is #{mb_img_size} #{mb_unit.upcase}. You have only #{free_allocation} #{mb_unit.upcase} / #{User::ALLOCATION_STRING} free" }
       # raise exception
-      render :text => result.to_json and return
+      return render :text => result.to_json
     end
 
     image_info = {
