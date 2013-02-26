64ed0a24228a33c58ad3d74d4f3ea1f46c9e0482
Phil Misiowiec <phil@webficient.com>
2013-02-25 21:30:45 -0800

    renamed dir

--- app/assets/javascripts/views/admin/flagged_images.js
+++ app/assets/javascripts/views/admin/flagged_images.js
@@ -1,70 +1,62 @@
-flagged_users = {
+var flagged_images = {
   setup_forms: function(){
     $('.action-container').delegate('.button.reinstate', 'click', function(e){
       var target = e.target;
       if(!$(target).hasClass('disabled')){
-        flagged_users.do_action(target);
+        flagged_images.do_action(target);
       }
     });
-    
+
     $('.action-container').delegate('.button.remove', 'click', function(e){
       var target = e.target;
       if(!$(target).hasClass('disabled')){
-        flagged_users.do_action(target);
+        flagged_images.do_action(target);
       }
     });
-    
-    $('.action-container').delegate('#remove_flagged_users', 'click', function(e){
+
+    $('.action-container').delegate('#remove_flagged_images', 'click', function(e){
       var target = e.target;
       if(!$(target).hasClass('disabled')){
-        flagged_users.do_action(target);
+        flagged_images.do_action(target);
       }
     });
-    
-    $('.action-container').delegate('#reinstate_flagged_users', 'click', function(e){
+
+    $('.action-container').delegate('#reinstate_flagged_images', 'click', function(e){
       var target = e.target;
       if(!$(target).hasClass('disabled')){
-        flagged_users.do_action(target);
+        flagged_images.do_action(target);
       }
     });
-    
-    // Setup tool tip, require jquery.poshytip
-    $('.button.tooltip').poshytip({
-      className: 'tip-yellowsimple',
-      showTimeout: 100,
-      alignTo: 'target',
-      alignX: 'center',
-      offsetY: 5,
-      allowTipHover: false
-    });
+
+    flagged_images.setup_image_popup();
   },
-  
+
   do_action: function(user){
-    $('#flagged-users-confirm-popup').modal();
-    var message = $(user).attr('data-confirm');    
+    $('#flagged-images-confirm-popup').modal();
+    var message = $(user).attr('data-confirm');
     // Change the message on modal dialog.
-    $('#flagged-users-confirm-popup').find('#confirm-message-container').text(message);
-    
+    $('#flagged-images-confirm-popup').find('#confirm-message-container').text(message);
+
     $('#btn-ok').click(function(){
       $.modal.close();
       window.setTimeout("$('#mask').modal()", 200);
       var form = $(user).find('form.action-form');
       var post_url = $(form).attr('action');
       var params = $(form).serialize();
-      
+
       if(!post_url || !params)
         return false;
-        
+
       $.ajax({
         url: post_url,
         type: 'POST',
         data: params,
         success: function(response){
           var need_close_loading = true;
-          
+
           if(response.status == 'ok'){
             $(user).addClass('disabled');
-            
+
             var redirect_url = $.trim(response.redirect_url);
             if(redirect_url && redirect_url != ''){
               need_close_loading = false;
@@ -76,11 +68,46 @@ flagged_users = {
               helper.show_notification(response.message);
             }
           }
-          
+
           if(need_close_loading)
-            $.modal.close(); 
+            $.modal.close();
         }
       });
     });
+  },
+
+  setup_image_popup: function(){
+    $(".image_frame, .flagged_image_container .image_name .image_link").click(function(e){
+      e.preventDefault();
+
+      var target_url = $.trim($(this).attr('data-url'));
+
+      if(target_url == '')
+        return false;
+
+      $("#mask").modal();
+
+      $.ajax({
+        url: target_url,
+        type: "GET",
+        dataType: "json",
+        success: function(data){
+          $.modal.close();
+          if (data.success) {
+            setTimeout(flagged_images.show_popup, 100);
+            $('#flagged_image_popup').html(data.data);
+          }else{
+            helper.show_notification(data.msg);
+          }
+        },
+        error: function(){
+          $.modal.close();
+        }
+      });
+    });
+  },
+
+  show_popup: function(){
+    $('#flagged_image_popup').modal();
   }
 };
--- app/assets/javascripts/views/admin/spotlights.js
+++ app/assets/javascripts/views/admin/spotlights.js
@@ -1,11 +1,11 @@
-spotlights = {
+var spotlights = {
   setup_promote_popup: function(){
     $('.image-container').delegate('.promote-button', 'click', function(e){
       var target = e.target;
       spotlights.do_promote_photo(target);
     });
   },
-  
+
   do_promote_photo: function(photo){
     $('#promote-confirm-popup').modal();
     var message = "";
@@ -18,20 +18,20 @@ spotlights = {
       message = $('#promote-confirm-popup-container').attr('data-promote-confirm');
       on_promoting = true;
     }
-    
+
     // Change the message on modal dialog.
     $('#promote-confirm-popup').find('#confirm-message-container').text(message);
-    
+
     $('#btn-ok').click(function(){
       $.modal.close();
       window.setTimeout("$('#mask').modal()", 200);
       var form = $(photo).find('form.promote-photo-form');
       var post_url = $(form).attr('action');
       var params = $(form).serialize();
-      
+
       if(!post_url || !params)
         return false;
-        
+
       $.ajax({
         url: post_url,
         type: 'POST',
@@ -40,7 +40,7 @@ spotlights = {
           if(response.status == 'ok'){
             var url = $.url(post_url);
             var params = url.param();
-            
+
             if(on_promoting){
               $(photo).addClass('disabled');
               // Change parent container style
@@ -52,7 +52,7 @@ spotlights = {
               $(photo).parents(".image-container").removeClass('promoted');
               delete params['demote'];
             }
-            
+
             post_url = url.attr('path') + "?" + $.param(params);
             // Change the form action URL.
             $(form).attr('action', post_url);
@@ -61,8 +61,8 @@ spotlights = {
             if(jQuery.trim(response.message) != ''){
               helper.show_notification(response.message);
             }
-          }         
-          
+          }
+
           $.modal.close();
         }
       });
