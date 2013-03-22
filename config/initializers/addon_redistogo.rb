2a44cbd820df2b01e6b1194cabc76c57300cce5e
Phil Misiowiec <phil@webficient.com>
2013-03-22 13:20:17 -0700

    removing

--- config/initializers/addon_redistogo.rb
+++
@@ -1,2 +0,0 @@
-# only use RedisToGo in stg/prod; use local version of Redis in dev/test
-ENV["REDISTOGO_URL"] = 'redis://username:password@domain.com:port/' if Rails.env.production? || Rails.env.staging?
