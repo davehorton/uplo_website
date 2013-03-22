a09a1dc48d83d2c45d8097f79b4cb84a89e3bc6e
Phil Misiowiec <phil@webficient.com>
2013-03-22 13:27:37 -0700

    changed token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Uplo::Application.config.secret_token = 'c667cc85f53eeb9355edee931997f68f59d626fa9413348169041c476935a336ce0c0c46a99ef2416d848f02052e605768aacf5481315630ae6e55230db9aa51'
+Uplo::Application.config.secret_token = '3d014c938e3a73427a3fee9b767c089e0106a70f4f05540f51a1bb27283ac89555883d2b63c83840bfe8f0bc1a7f3fb4e4ba1e65b81eca350ee9ef819d6c2ed9'
