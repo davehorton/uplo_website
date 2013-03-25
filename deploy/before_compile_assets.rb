88cd8f278b2258a1fb63dad41a71e6062e11b230
Phil Misiowiec <phil@webficient.com>
2013-03-25 04:43:22 -0700

    symlink

--- deploy/before_compile_assets.rb
+++ deploy/before_compile_assets.rb
@@ -1,3 +1,3 @@
-%w(amazon_s3 social).each do |file_name|
+%w(amazon_s3 redis social).each do |file_name|
   run "ln -nfs #{shared_path}/config/#{file_name}.yml #{release_path}/config/#{file_name}.yml"
 end
