0c79e2e63c58228727951fc6379684cbe2519c5d
Phil Misiowiec <phil@webficient.com>
2013-03-25 04:39:22 -0700

    restart sidekiq

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,2 +1,5 @@
+# ensures Sidekiq workers are using latest version of the app
+sudo "monit -g uplo_web_sidekiq restart all"
+
 # ensures memcached is not stale
 run "cd #{current_path} && bundle exec rake cache:clear"
