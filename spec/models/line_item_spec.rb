be902316cacd14b3e3727bda8eb61aefd1b063f8
Sathi Patranabish <spatranabish@kreeti.com>
2013-03-26 18:31:36 +0530

    fixed mixed errors while running specs

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -7,19 +7,6 @@ describe LineItem do
 
   it { should validate_numericality_of(:quantity) }
 
-  describe "#total_price" do
-    it "should be calculated" do
-      line_item =  create(:line_item, :price => 500, :quantity => 5)
-      line_item.total_price.should == 2500
-    end
-  end
 
-  describe "#calculate_totals" do
-    it "should return calculated result" do
-      new_line_item = create(:line_item, :price => 500)
-      new_line_item.calculate_totals
-      new_line_item.tax.should be_zero
-    end
-  end
 end
 
