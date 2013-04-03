a04c100f494229ee374fc83c416fbd4c75be85dc
Phil Misiowiec <phil@webficient.com>
2013-04-02 18:51:32 -0700

    corrected tier => tier_id

--- app/serializers/image_serializer.rb
+++ app/serializers/image_serializer.rb
@@ -1,6 +1,6 @@
 class ImageSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :image_file_name, :name, :gallery_id,
-             :price, :image_likes_count, :keyword, :owner_avatar, :gallery_cover, :tier,
+             :price, :image_likes_count, :keyword, :owner_avatar, :gallery_cover, :tier_id,
              :image_url, :image_thumb_url, :username, :creation_timestamp, :user_fullname,
              :public_link, :user_id, :user_avatar, :comments_number, :gallery_name
 
