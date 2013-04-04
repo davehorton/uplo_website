7ebd1b651413fb581bc6da4187ed87b1ec2c0a99
Sathi Patranabish <spatranabish@kreeti.com>
2013-04-04 12:33:26 +0530

    [Fixes, #47423595] git commit -m [Fixes,

--- spec/models/moulding_spec.rb
+++ spec/models/moulding_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Moulding do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should respond_to(:name) }
+
+  it { should validate_presence_of(:name) }
+
+  it "has a default sort of moulding ids" do
+    moulding1 = create(:moulding)
+    moulding2 = create(:moulding)
+    klass.all.should == [moulding1, moulding2]
+  end
+
+  describe ".in_product" do
+    it "should return appropriate moulding" do
+      moulding = create(:moulding)
+      product = create(:product, :moulding_id => moulding.id)
+      Moulding.in_product.should == [moulding]
+    end
+  end
+
 end
