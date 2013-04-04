1e00f178a3413ea904fcbcc52423bdac91d6e1d7
Sathi Patranabish <spatranabish@kreeti.com>
2013-04-04 18:32:47 +0530

    refs # 47423595, used let

--- spec/models/moulding_spec.rb
+++ spec/models/moulding_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
 describe Moulding do
+  let(:moulding) { create(:moulding) }
+
   it { should respond_to(:name) }
 
   it { should validate_presence_of(:name) }
 
   it "has a default sort of moulding ids" do
     moulding1 = create(:moulding)
-    moulding2 = create(:moulding)
-    klass.all.should == [moulding1, moulding2]
+    klass.all.should == [moulding1]
   end
 
   describe ".in_product" do
     it "should return appropriate moulding" do
-      moulding = create(:moulding)
       product = create(:product, :moulding_id => moulding.id)
       Moulding.in_product.should == [moulding]
     end
