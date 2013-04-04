082d0573ceb20d7da699ec08e10ae59c1394e781
Sathi Patranabish <spatranabish@kreeti.com>
2013-04-04 19:11:53 +0530

    refs #47430731, used let to simplify specs

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe Order do
-  let(:order) { create(:order) }
+  let(:empty_order) { create(:order) }
+  let(:order) { create(:order_with_line_items) }
 
   it { should belong_to(:user) }
   it { should belong_to(:shipping_address) }
@@ -19,26 +20,26 @@ describe Order do
   it { should validate_numericality_of(:card_number).only_integer }
 
   it "has a completed scope" do
-    order1 = create(:order, :transaction_status => "completed")
-    Order.completed.should == [order1]
+    empty_order.update_attribute(:transaction_status, "completed")
+    Order.completed.should == [empty_order]
   end
 
   describe "#update_tax_by_state" do
     context "when no has tax" do
       it "should update tax to zero" do
-        order.update_tax_by_state
-        order.tax.should be_zero
+        empty_order.update_tax_by_state
+        empty_order.tax.should be_zero
       end
     end
 
     context "with with shipping address" do
       it "should update tax" do
         shipping_address = create(:shipping_address, :state => "new york")
-        order1 = create(:order_with_line_items, :shipping_address_id => shipping_address.id)
-        line_item = order1.line_items.first
+        order.shipping_address_id = shipping_address.id
+        line_item = order.line_items.first
         line_item.update_attributes(:price => 500, :quantity => 4)
-        order1.update_tax_by_state
-        order1.tax.should == 355.0
+        order.update_tax_by_state
+        order.tax.should == 355.0
       end
     end
   end
@@ -55,7 +56,7 @@ describe Order do
 
     context "without line items" do
       it "should return result" do
-        order.compute_image_total.should be_zero
+        empty_order.compute_image_total.should be_zero
       end
     end
   end
@@ -63,20 +64,19 @@ describe Order do
   describe "#compute_totals" do
     context "with line items" do
       it "should return result" do
-        order1 = create(:order_with_line_items)
-        line_item = order1.line_items.first
+        line_item = order.line_items.first
         line_item.update_attributes(:price => 500, :quantity => 4)
-        order1.compute_totals
-        order1.price_total.to_i.should == 4000
-        order1.order_total.to_i.should == 4015
+        order.compute_totals
+        order.price_total.to_i.should == 4000
+        order.order_total.to_i.should == 4015
       end
     end
 
     context "without line items" do
       it "should return result" do
-        order.compute_totals
-        order.price_total.should be_zero
-        order.order_total.to_i.should == 15
+        empty_order.compute_totals
+        empty_order.price_total.should be_zero
+        empty_order.order_total.to_i.should == 15
       end
     end
   end
@@ -84,14 +84,14 @@ describe Order do
   describe "#transaction_completed?" do
     context "with completed status" do
       it "should return true" do
-        order1 = create(:order, :transaction_status => "completed")
-        order1.transaction_completed?.should be_true
+        empty_order.update_attribute(:transaction_status, "completed")
+        empty_order.transaction_completed?.should be_true
       end
     end
 
     context "with not completed status" do
       it "should return false" do
-        order.transaction_completed?.should be_false
+        empty_order.transaction_completed?.should be_false
       end
     end
   end
