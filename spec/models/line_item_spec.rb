9008b3d5d25e21027e0059e63212a13a0e1e6af7
Sathi Patranabish <spatranabish@kreeti.com>
2013-04-05 12:40:26 +0530

    refs #47439687, modified changes

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -2,8 +2,7 @@ require 'spec_helper'
 require 'sidekiq/testing/inline'
 
 describe LineItem do
-  let(:image) { create(:image, :tier_id => 1) }
-  let(:empty_line_item) { create(:line_item, :image_id => image.id) }
+  let(:line_item) { create(:line_item, image: create(:real_image))}
 
   it { should belong_to(:image) }
   it { should belong_to(:product) }
@@ -14,9 +13,8 @@ describe LineItem do
   describe ".sold_items" do
     context "with matching orders" do
       it "should return appropriate line items" do
-        new_order = create(:order, :transaction_status => "completed")
-        line_item1 = create(:line_item, :image_id => image.id, :order_id => new_order.id)
-        LineItem.sold_items.should == [line_item1]
+        line_item.order.update_attribute(:transaction_status, "completed")
+        LineItem.sold_items.should == [line_item]
       end
     end
 
@@ -29,17 +27,17 @@ describe LineItem do
 
   describe "total_price" do
     it "should calculate total" do
-      empty_line_item.update_attributes(:tax => 10.0, :quantity => 4, :price => 500)
-      empty_line_item.total_price.should == 2000
+      line_item.update_attributes(:tax => 10.0, :quantity => 4, :price => 500)
+      line_item.total_price.should == 2000
     end
   end
 
   describe "#calculate_totals" do
     context "executing before_save calback" do
       it "should calculate result" do
-        empty_line_item.price.should == 500
-        empty_line_item.tax.should be_zero
-        empty_line_item.commission_percent.should == 100
+        line_item.price.should == 500
+        line_item.tax.should be_zero
+        line_item.commission_percent.should == 100
       end
     end
   end
@@ -71,6 +69,27 @@ describe LineItem do
       line_item.url.should_not be_blank
       file_path = Rails.root.to_s + "/public" + line_item.url.split('?').first
       Paperclip::Geometry.from_file(file_path).to_s.should == "888x888"
+    end
+  end
+
+  describe "#cropping?" do
+    context "with cropping parameters" do
+      it "should return true" do
+        line_item.update_attributes(:crop_x => 40, :crop_y => 50, :crop_h => 40, :crop_w => 50)
+        line_item.cropping?.should be_true
+      end
+    end
+
+    context "without cropping parameters" do
+      it "should return false" do
+        line_item.cropping?.should be_false
+      end
+    end
+  end
+
+  describe "#s3_expire_time" do
+    it "should return proper time" do
+      line_item.s3_expire_time.should == "#{Time.zone.now.beginning_of_day.since 25.hours}"
     end
   end
 
