07c3232e68e7f2a3d727c48285a909bd35b72511
Sathi Patranabish <spatranabish@kreeti.com>
2013-04-18 15:52:51 +0530

    minor modifications

--- config/initializers/dropbox_account.rb
+++ config/initializers/dropbox_account.rb
@@ -1,2 +1,4 @@
 DROPBOX = YAML.load_file(File.join(Rails.root, "config", "dropbox.yml"))[Rails.env]
+Dropbox::API::Config.app_key = 'l0rj7yhvm6f0jtj'
+Dropbox::API::Config.app_secret = 'spmfic5xcltg06i'
 Dropbox::API::Config.mode = "sandbox"
