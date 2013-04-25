66b2fcbb8fc945764d0a348a0a2ee7ebf8c6d9c5
Sathi Patranabish <spatranabish@kreeti.com>
2013-04-25 11:39:02 +0530

    migration added

---
+++ db/migrate/20130423111941_create_gallery_invitations.rb
@@ -0,0 +1,14 @@
+class CreateGalleryInvitations < ActiveRecord::Migration
+  def change
+    create_table :gallery_invitations do |t|
+      t.integer :gallery_id, :null => false
+      t.string :email, :null => false, :unique => true
+      t.string :secret_token, :null => false
+      t.boolean :accepted, :default => false
+      t.string :message
+      t.timestamps
+    end
+    add_foreign_key(:gallery_invitations, :galleries)
+    add_index :gallery_invitations, :secret_token
+  end
+end
