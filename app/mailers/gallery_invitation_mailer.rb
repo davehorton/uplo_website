6853b3f1263753fafc6fed9167d2b7f6d4e53220
Phil Misiowiec <phil@webficient.com>
2013-05-07 09:46:54 -0700

    use correct attribute

--- app/mailers/gallery_invitation_mailer.rb
+++ app/mailers/gallery_invitation_mailer.rb
@@ -3,10 +3,9 @@ class GalleryInvitationMailer < ActionMailer::Base
   def send_invitation(invitation_id)
     @gallery_invitation = GalleryInvitation.find(invitation_id)
     mail(
-         to: @gallery_invitation.email,
-         subject: 'UPLO Invite to Gallery',
-         from: @gallery_invitation.gallery.user.fullname
-       )
+      to: @gallery_invitation.email,
+      subject: 'UPLO Invite to Gallery',
+      from: @gallery_invitation.gallery.user.friendly_email
+    )
   end
-
 end
