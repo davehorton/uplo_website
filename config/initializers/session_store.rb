cc2dadc4947e558e1eecd0ed5f7b9866d0838a4e
Phil Misiowiec <phil@webficient.com>
2013-05-08 07:36:11 -0700

    AR sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Uplo::Application.config.session_store :cookie_store, key: '_uplo_session', :domain => :all
+Uplo::Application.config.session_store :active_record_store, key: '_uplo_session', :domain => :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130507164911) do
+ActiveRecord::Schema.define(:version => 20130508142641) do
 
   create_table "addresses", :force => true do |t|
     t.string   "first_name"
@@ -251,6 +251,16 @@ ActiveRecord::Schema.define(:version => 20130507164911) do
 
   add_index "profile_images", ["link_to_image"], :name => "index_profile_images_on_link_to_image"
   add_index "profile_images", ["user_id"], :name => "index_profile_images_on_user_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "sizes", :force => true do |t|
     t.integer  "width"
