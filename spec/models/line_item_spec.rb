8ecd05f3c5c87fbc25d62e4511e16027a83cbac2
Sathi Patranabish <spatranabish@kreeti.com>
2013-05-24 15:43:58 +0530

    [#50048639] reverted

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -5,11 +5,8 @@ describe LineItem do
   before { create_sizes }
   let(:line_item) { create(:line_item, image: create(:image))}
 
-  it { should respond_to(:generate_ordered_print) }
-
   it { should belong_to(:image) }
   it { should belong_to(:product) }
-  it { should belong_to(:product_option) }
   it { should belong_to(:order) }
 
   it { should validate_numericality_of(:quantity) }
@@ -43,6 +40,41 @@ describe LineItem do
         line_item.price.should == 500
         line_item.tax.should be_zero
         line_item.commission_percent.should == 100
+      end
+    end
+  end
+
+  describe "#set_crop_dimension" do
+    it "should set crop dimension field from a hash" do
+      options = {crop_x: "1445", crop_y: "312", crop_w: "888", crop_h: "888"}
+      line_item.set_crop_dimension(options)
+      line_item.crop_dimension.should == "888x888+1445+312"
+    end
+  end
+
+  describe "#cropped_dimensions" do
+    it "should return an empty array" do
+      line_item.cropped_dimensions.should == []
+    end
+
+    it "should not return an empty array" do
+      options = {crop_x: "1445", crop_y: "312", crop_w: "888", crop_h: "888"}
+      line_item.set_crop_dimension(options)
+      line_item.cropped_dimensions.should == [888, 888, 1445, 312]
+    end
+  end
+
+  describe "#cropping?" do
+    context "with cropping parameters" do
+      it "should return true" do
+        line_item.update_attributes(:crop_x => 40, :crop_y => 50, :crop_h => 40, :crop_w => 50)
+        line_item.cropping?.should be_true
+      end
+    end
+
+    context "without cropping parameters" do
+      it "should return false" do
+        line_item.cropping?.should be_false
       end
     end
   end
