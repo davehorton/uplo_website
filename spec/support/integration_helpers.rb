4fc5fd6b844df1a980d598adbc4b9644e3d3f0e9
Sathi Patranabish <spatranabish@kreeti.com>
2013-06-27 15:59:36 +0530

    [#46799687] completed accessing user profile

--- spec/support/integration_helpers.rb
+++ spec/support/integration_helpers.rb
@@ -1,3 +1,12 @@
 module IntegrationHelpers
 
+  def login_user(user = nil)
+    visit signin_path
+    within("#new_user") do
+      fill_in "user_login", :with => user.email
+      fill_in "user_password", :with => user.password
+      click_on "Sign in"
+    end
+  end
+
 end
