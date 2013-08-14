d92ce78bf83e2b3465ff88abbaa84497c9956ebb
sathi210 <spatranabish@kreeti.com>
2013-08-14 13:44:49 +0530

    [#54963980] added mailer specs

--- spec/mailers/payment_mailer_spec.rb
+++ spec/mailers/payment_mailer_spec.rb
@@ -1,5 +1,14 @@
 require "spec_helper"
 
 describe PaymentMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { ActionMailer::Base.deliveries = [] }
+
+  it "should send inform order email after an order is completed" do
+    order = create(:order, :status => "shopping", :tax => 10.0)
+    cart = create(:cart, :order_id => order.id)
+    order.finalize_transaction
+    ActionMailer::Base.deliveries.should_not be_empty
+  end
+
 end
+
