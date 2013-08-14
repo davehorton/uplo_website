4e2343f4fb91764f9ab8628ca6f74798d8f6a649
sathi210 <spatranabish@kreeti.com>
2013-08-14 14:36:49 +0530

    [#54963980] added mailer specs for investigating the email content bug

--- spec/mailers/payment_mailer_spec.rb
+++ spec/mailers/payment_mailer_spec.rb
@@ -1,27 +1,31 @@
 require "spec_helper"
 
 describe PaymentMailer do
-  let(:order) { create(:order, :status => "shopping", :tax => 10.0) }
-  let(:cart) { create(:cart, :order_id => order.id) }
+  let(:order) { create(:order, :status => "shopping", :tax => 10.0, :shipping_fee => 20.0) }
   let(:mail) { PaymentMailer.inform_new_order(order.id) }
 
   before { ActionMailer::Base.deliveries = [] }
 
   it "should send inform order email after an order is completed" do
+    cart = create(:cart, :order_id => order.id)
     order.finalize_transaction
     ActionMailer::Base.deliveries.should_not be_empty
   end
 
-  scenario "email details" do
-    mail.subject.should eq("Password Reset")
+  it "should match email details" do
+    mail.subject.should eq("New Order")
     mail.to.should eq(["patrick@uplo.com", "uplo@digital2media.com"])
     mail.from.should eq(["support@uplo.com"])
   end
 
-  scenrio "renders the body" do
-    mail.body.encoded.should have_selector("p", :text => "#{order.user.username} (#{order.user.fullname}, #{mail_to order.user.email}) purchased a photo on Uplo.com.")
+  it "renders the body" do
+    mail.body.encoded.should have_content("#{order.user.username} (#{order.user.fullname}")
+    mail.body.encoded.should have_selector("p", :text => "Shipping Address: #{order.ship_to_address}")
+    mail.body.encoded.should have_selector("h3", :text => "Order Summary")
+    mail.body.encoded.should have_content("Tax: $10.0")
+    mail.body.encoded.should have_content("S&H: $20.0")
+    mail.body.encoded.should have_selector("p", :text => "The cropped and original photos will appear shortly in the Uplo Dropbox subfolder named #{order.dropbox_order_root_path}.")
   end
-
 
 end
 
