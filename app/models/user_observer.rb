b314ec99f1802f88febaebb33fb2d8359f496873
Markus Fueger <markus@fueger.net>
2013-08-16 16:10:51 -0400

    [#55312414] with unique email no existing invitations should exist

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,7 +1,7 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
     # remove existing invites
-    Invitation.destroy_all(email: user.email)
+    # Invitation.destroy_all(email: user.email)
 
     # subscribe to newsletter in production
     # Mailchimp.delay.subscribe_user(user.id) if Rails.env.production?
