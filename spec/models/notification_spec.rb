c76305c2ad7b192ca3ecda5f37611eade8a1a3a7
sathi210 <spatranabish@kreeti.com>
2013-08-20 15:15:52 +0530

    [#55357038] updated failing specs

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -6,20 +6,30 @@ describe Notification do
 
   describe "action" do
     context "when type is a like" do
-      it "should return proper type" do
-        Notification.action(0, by_user.fullname).should == "john doe likes your image"
+      it "should return proper message when admin" do
+        by_user.update_attribute(:admin, true)
+        Notification.action(0, by_user).should == "UPLO likes your image"
+      end
+
+      it "should return proper message when non admin" do
+        Notification.action(0, by_user).should == "john doe likes your image"
       end
     end
 
     context "when type is a comment" do
-      it "should return proper type" do
-        Notification.action(1, by_user.fullname).should == "john doe left you a comment!"
+      it "should return proper message when admin" do
+        by_user.update_attribute(:admin, true)
+        Notification.action(1, by_user).should == "UPLO left you a comment!"
+      end
+
+      it "should return proper message when non admin" do
+        Notification.action(1, by_user).should == "john doe left you a comment!"
       end
     end
 
     context "when purchased" do
       it "should return proper type" do
-        Notification.action(2, by_user.fullname).should == "You just made a sale!"
+        Notification.action(2, by_user).should == "You just made a sale!"
       end
     end
   end
