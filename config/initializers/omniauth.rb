5ac055338b93d18d647cd56b829dba42143c3119
sathi210 <spatranabish@kreeti.com>
2013-08-27 16:25:01 +0530

    [#55809218] added rescue for omniauth failure

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"]
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: "publish_actions"
 end
+OmniAuth.config.on_failure = HomeController.action(:index)
