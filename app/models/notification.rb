1e8388d2277e557792f4df0d35a33b9257c45017
sathi210 <spatranabish@kreeti.com>
2013-09-04 17:22:49 +0530

    [#56072022] added new method and simplified notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -36,47 +36,27 @@ class Notification < ActiveRecord::Base
     end
   end
 
-  def self.deliver_comment_notification(image_id, by_user_id, type)
-    image = Image.find_by_id(image_id)
-    if image
-      by_user = User.find_by_id(by_user_id)
-      receivers = User.where(id: image.comments.where("user_id != ?", by_user_id).pluck(:user_id).uniq)
-
-      if by_user && receivers.present?
-
-        receivers.each do |receiver|
-          if receiver.device_tokens.present?
-
-            if by_user.admin?
-              message = "UPLO has commented on the same image"
-            else
-              message = "#{by_user.fullname} has commented on the same image"
-            end
-
-            notification = {
-              :schedule_for => [30.second.from_now],
-              :device_tokens => receiver.device_tokens,
-              :aps => { :alert => message },
-              :data => { :type => TYPE_ACTION[type].to_s, :id => image.id.to_s }
-            }
-            Urbanairship.push(notification)
-          end
-        end
-      end
+  def self.deliver_comment_notification(comment)
+    by_user = comment.user
+    receivers =  User.where(id: comment.image.comments.where("user_id != ?", by_user.id).pluck(:user_id).uniq).select {|u| u.device_tokens.present?}
+    receivers.each do |receiver|
+      notification = {
+        :schedule_for => [30.second.from_now],
+        :device_tokens => receiver.device_tokens,
+        :aps => { :alert => "#{by_user.name_for_notification} has commented on the same image" },
+        :data => { :type => "commented_on", :id => comment.image_id.to_s }
+      }
+      Urbanairship.push(notification)
     end
   end
 
   def self.deliver_spotlight_notification(image, type)
     receiver = image.user
-
     if receiver.device_tokens.present?
-
-      message = "Your image just made it to the Spotlight!"
-
       notification = {
         :schedule_for => [30.second.from_now],
         :device_tokens => receiver.device_tokens,
-        :aps => { :alert => message },
+        :aps => { :alert => "Your image just made it to the Spotlight!" },
         :data => { :type => TYPE_ACTION[type].to_s, :id => image.id.to_s }
         }
       Urbanairship.push(notification)
@@ -86,19 +66,11 @@ class Notification < ActiveRecord::Base
   def self.deliver_follow_notification(user_follow, type)
     follower = User.find_by_id(user_follow.followed_by)
     receiver = user_follow.followed_user
-
     if receiver.device_tokens.present?
-
-      if follower.admin?
-        message = "UPLO has just started following you"
-      else
-        message = "#{follower.username} has just started following you"
-      end
-
       notification = {
         :schedule_for => [30.second.from_now],
         :device_tokens => receiver.device_tokens,
-        :aps => { :alert => message },
+        :aps => { :alert => "#{follower.name_for_notification} has just started following you" },
         :data => { :type => TYPE_ACTION[type].to_s, :id => follower.id.to_s }
       }
       Urbanairship.push(notification)
@@ -106,11 +78,7 @@ class Notification < ActiveRecord::Base
   end
 
   def self.action(type, by_user)
-    if by_user.admin?
-      fullname = "UPLO"
-    else
-      fullname = by_user.fullname
-    end
+    fullname = by_user.name_for_notification
 
     case type
       when TYPE[:like] then "#{fullname} likes your image"
