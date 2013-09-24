422f5d387bac0007acd1a2742bd3139fd77054f9
Jeff Bennett <jeff@webficient.com>
2013-09-24 13:57:51 -0700

    [#57601636] attempt to open raffle in popup or same window

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Uplo::Application.routes.draw do
   get "shopping_cart/clear"
   get "shopping_cart/checkout"
 
+  get "raffle", :to => "home#raffle"
   get "browse", :to => "home#browse"
   get "require_login", :to => "home#require_login"
   get "search", :to => "home#search"
