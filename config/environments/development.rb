eda0eff50f6e9adb6f5155afb8c5bf1435cbe658
Santanu Karmakar <skarmakar@kreeti.com>
2013-10-16 18:08:57 +0530

    [Fixes #58890656] revert dev config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Uplo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  ENV['UPLO_DOMAIN'] = 'uplo.dev:3000'
+  ENV['UPLO_DOMAIN'] = 'uplo.dev'
   DOMAIN = ENV['UPLO_DOMAIN'] || 'localhost:3000'
 
   # In the development environment your application's code is reloaded on
