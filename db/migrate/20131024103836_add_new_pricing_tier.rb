fa41d184421d6e4e6d5b2494b334e3c2c8daaa61
sathi210 <spatranabish@kreeti.com>
2013-10-28 13:04:45 +0530

    [#58888958] added price for remaining tiers

--- db/migrate/20131024103836_add_new_pricing_tier.rb
+++ db/migrate/20131024103836_add_new_pricing_tier.rb
@@ -14,28 +14,52 @@ class AddNewPricingTier < ActiveRecord::Migration
     # insert tier 3 price for all products
     mouldings = {
       "Print Only (Gloss)" => [
+                               { height: 4, width: 4, price: 5 },
+                               { height: 4, width: 6, price: 5 },
+                               { height: 5, width: 5, price: 10 },
+                               { height: 5, width: 7, price: 10 },
+                               { height: 8, width: 8, price: 150 },
                                { height: 8, width: 10, price: 150 },
+                               { height: 12, width: 12, price: 400 },
                                { height: 12, width: 16, price: 400 },
+                               { height: 20, width: 20, price: 800 },
                                { height: 20, width: 24, price: 800 },
+                               { height: 24, width: 24, price: 1100 },
                                { height: 24, width: 36, price: 1100 }
                               ],
 
       "Print Only (Luster)" => [
+                                { height: 4, width: 4, price: 5 },
+                                { height: 4, width: 6, price: 5 },
+                                { height: 5, width: 5, price: 10 },
+                                { height: 5, width: 7, price: 10 },
+                                { height: 8, width: 8, price: 150 },
                                 { height: 8, width: 10, price: 150 },
+                                { height: 12, width: 12, price: 400 },
                                 { height: 12, width: 16, price: 400 },
+                                { height: 20, width: 20, price: 800 },
                                 { height: 20, width: 24, price: 800 },
+                                { height: 24, width: 24, price: 1100 },
                                 { height: 24, width: 36, price: 1100 }
                                ],
       "Canvas" => [
+                   { height: 8, width: 8, price: 150 },
                    { height: 8, width: 10, price: 150 },
+                   { height: 12, width: 12, price: 400 },
                    { height: 12, width: 16, price: 400 },
+                   { height: 20, width: 20, price: 600 },
                    { height: 20, width: 24, price: 600 },
+                   { height: 24, width: 24, price: 1000 },
                    { height: 24, width: 36, price: 1000 }
                   ],
       "Plexi" =>  [
+                   { height: 8, width: 8, price: 300 },
                    { height: 8, width: 10, price: 300 },
+                   { height: 12, width: 12, price: 550 },
                    { height: 12, width: 16, price: 550 },
+                   { height: 20, width: 20, price: 1000 },
                    { height: 20, width: 24, price: 1000 },
+                   { height: 24, width: 24, price: 1500 },
                    { height: 24, width: 36, price: 1500 }
                   ]
     }
@@ -44,8 +68,14 @@ class AddNewPricingTier < ActiveRecord::Migration
       moulding = Moulding.find_by_name(moulding_name)
       sizes.each do |options|
         size = Size.where(height: options[:height], width: options[:width]).first
-        product = moulding.products.where(size_id: size.id).first
-        product.update_attributes!(tier3_price: options[:price], tier3_commission: 35) if product
+        if size
+          product = moulding.products.where(size_id: size.id).first
+          if product.present?
+            product.update_attributes!(tier3_price: options[:price], tier3_commission: 35)
+          else
+            puts "Product not available for size #{size.to_name} and  moulding #{moulding.name}"
+          end
+        end
       end
     end
 
