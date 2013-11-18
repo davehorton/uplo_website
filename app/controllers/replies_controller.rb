a5ce859d6409072338898a903b8c1ccd5cbf8891
sathi210 <spatranabish@kreeti.com>
2013-11-18 16:59:52 +0530

    [#58886538] added specs for image comment action

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -6,6 +6,7 @@ class RepliesController < ApplicationController
     user = User.find_by_email(params[:from])
     parsed_body = EmailReplyParser.parse_reply(params[:text])
     user.comments.create!(description: parsed_body, image_id: image_id)
+    redirect_to root_path
   end
 
 end
