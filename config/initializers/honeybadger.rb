710daaf3c2f8f523532ba1b2a5d0d6278e85b6ca
Phil Misiowiec <phil@webficient.com>
2014-04-25 20:35:50 -0700

    updated honey badger

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,3 +1,8 @@
 Honeybadger.configure do |config|
   config.api_key = 'e1521a6d'
+  config.params_filters << "card_type"
+  config.params_filters << "name_on_card"
+  config.params_filters << "cvv"
+  config.params_filters << "expiration"
+  config.params_filters << "4750556078184269"
 end
